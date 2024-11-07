be190f7026170aa2bae1b1fea6fe2da9644a7f64
kouliann <elianniebth.uni@gmail.com>
2024-11-06 21:54:19 -0400

    DJKDKS

---
+++ nuevo-proyecto-master/javascript/alerts.js
@@ -0,0 +1,17 @@
+/*alert("hola que tal");*/
+
+var n1=parseInt(prompt("ingrese el valor de n1", 0));
+var n2=parseInt(prompt("Ingrese el valor de n2", 0));
+
+console.log (n1,n2);
+
+if(n1>n2){
+    console.log(n1+ "es mayor que " +n2);
+}
+else if(n1==n2){
+    console.log (n2+ "es igual a" +n1);
+}
+else{
+    console.log(n2+ "es menor que " +n1);
+
+}
